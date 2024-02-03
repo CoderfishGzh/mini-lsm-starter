ee43805f60c281b3dba3476f22abee670dd8574f
CoderfishGzh <335247945@qq.com>
2024-02-04 00:26:33 +0800

    modify merge_iter

--- src/iterators/merge_iterator.rs
+++ src/iterators/merge_iterator.rs
@@ -2,6 +2,7 @@
 #![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
 
 use std::cmp::{self};
+use std::collections::binary_heap::PeekMut;
 use std::collections::BinaryHeap;
 
 use anyhow::Result;
@@ -12,42 +13,64 @@ use super::StorageIterator;
 
 struct HeapWrapper<I: StorageIterator>(pub usize, pub Box<I>);
 
-// impl<I: StorageIterator> PartialEq for HeapWrapper<I> {
-//     fn eq(&self, other: &Self) -> bool {
-//         self.partial_cmp(other).unwrap() == cmp::Ordering::Equal
-//     }
-// }
+impl<I: StorageIterator> PartialEq for HeapWrapper<I> {
+    fn eq(&self, other: &Self) -> bool {
+        self.partial_cmp(other).unwrap() == cmp::Ordering::Equal
+    }
+}
 
-// impl<I: StorageIterator> Eq for HeapWrapper<I> {}
+impl<I: StorageIterator> PartialOrd for HeapWrapper<I> {
+    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+        match self.1.key().cmp(&other.1.key()) {
+            cmp::Ordering::Equal => self.0.partial_cmp(&other.0),
+            cmp::Ordering::Greater => Some(cmp::Ordering::Greater),
+            cmp::Ordering::Less => Some(cmp::Ordering::Less),
+        }
+        .map(|x| x.reverse())
+    }
+}
 
-// impl<I: StorageIterator> PartialOrd for HeapWrapper<I> {
-//     #[allow(clippy::non_canonical_partial_ord_impl)]
-//     fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
-//         match self.1.key().cmp(&other.1.key()) {
-//             cmp::Ordering::Greater => Some(cmp::Ordering::Greater),
-//             cmp::Ordering::Less => Some(cmp::Ordering::Less),
-//             cmp::Ordering::Equal => self.0.partial_cmp(&other.0),
-//         }
-//         .map(|x| x.reverse())
-//     }
-// }
+impl<I: StorageIterator> Eq for HeapWrapper<I> {}
 
-// impl<I: StorageIterator> Ord for HeapWrapper<I> {
-//     fn cmp(&self, other: &Self) -> cmp::Ordering {
-//         self.partial_cmp(other).unwrap()
-//     }
-// }
+impl<I: StorageIterator> Ord for HeapWrapper<I> {
+    fn cmp(&self, other: &Self) -> cmp::Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
 
 /// Merge multiple iterators of the same type. If the same key occurs multiple times in some
 /// iterators, perfer the one with smaller index.
 pub struct MergeIterator<I: StorageIterator> {
     iters: BinaryHeap<HeapWrapper<I>>,
-    current: HeapWrapper<I>,
+    current: Option<HeapWrapper<I>>,
 }
 
 impl<I: StorageIterator> MergeIterator<I> {
     pub fn create(iters: Vec<Box<I>>) -> Self {
-        unimplemented!()
+        if iters.is_empty() {
+            return Self {
+                iters: BinaryHeap::new(),
+                current: None,
+            };
+        }
+
+        let mut heap = BinaryHeap::new();
+        for (i, it) in iters.into_iter().enumerate() {
+            if it.is_valid() {
+                heap.push(HeapWrapper(i, it));
+            }
+        }
+
+        match heap.pop() {
+            Some(x) => Self {
+                iters: heap,
+                current: Some(x),
+            },
+            None => Self {
+                iters: heap,
+                current: None,
+            },
+        }
     }
 }
 
@@ -57,18 +80,54 @@ impl<I: 'static + for<'a> StorageIterator<KeyType<'a> = KeySlice<'a>>> StorageIt
     type KeyType<'a> = KeySlice<'a>;
 
     fn key(&self) -> KeySlice {
-        unimplemented!()
+        self.current.as_ref().unwrap().1.key()
     }
 
     fn value(&self) -> &[u8] {
-        unimplemented!()
+        self.current.as_ref().unwrap().1.value()
     }
 
     fn is_valid(&self) -> bool {
-        unimplemented!()
+        self.current
+            .as_ref()
+            .map(|x| x.1.is_valid())
+            .unwrap_or(false)
     }
 
     fn next(&mut self) -> Result<()> {
-        unimplemented!()
+        let current = self.current.as_mut().unwrap();
+        // delete all the same key
+        while let Some(mut inner_iter) = self.iters.peek_mut() {
+            if inner_iter.1.key() == current.1.key() {
+                // case1: call next err
+                if let e @ Err(_) = inner_iter.1.next() {
+                    PeekMut::pop(inner_iter);
+                    return e;
+                }
+
+                // case2: iter unvalid
+                if !inner_iter.1.is_valid() {
+                    PeekMut::pop(inner_iter);
+                }
+            } else {
+                break;
+            }
+        }
+
+        current.1.next()?;
+
+        if !current.1.is_valid() {
+            if let Some(e) = self.iters.pop() {
+                *current = e;
+            }
+        }
+
+        if let Some(mut inner_iter) = self.iters.peek_mut() {
+            if *current < *inner_iter {
+                std::mem::swap(&mut *inner_iter, current);
+            }
+        }
+
+        Ok(())
     }
 }
